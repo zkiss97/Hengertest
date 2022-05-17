85774278f9c1b1de251298a9a2f5d763f6be1ab5
zkiss97 <zkiss1997@gmail.com>
2022-05-17 11:40:26 +0200

    test

---
+++ HengertestTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HengertestTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
